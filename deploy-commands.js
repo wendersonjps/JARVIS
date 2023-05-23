c01961e8bec17a58434f03d328b1922976e494af
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-23 16:41:46 -0300

    :sparkles: feat: Created deploy commands file

---
+++ deploy-commands.js
@@ -0,0 +1,38 @@
+const { REST, Routes } = require('discord.js')
+
+// .env File
+const dotenv = require('dotenv')
+dotenv.config()
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+
+// Commands Import
+const fs = require('node:fs')
+const path = require('node:path')
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+
+const commands = []
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+}
+
+// REST Instance
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// Deploy
+(async () => {
+    try {
+        console.log(`Resetando ${commands.length} comandos...`)
+
+        // PUT
+        const data = await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands }
+        )
+        console.log('Comandos registrados com sucesso!')
+    } catch (error) {
+        console.error(error)
+    }
+})()
